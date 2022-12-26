9910b951487700cf9396e5ce6ec01c295676835e
herukhoeruman <herukhoeruman110@gmail.com>
2022-12-26 17:00:55 +0700

    payment api midtrans endpoint

--- handler/transaction.go
+++ handler/transaction.go
@@ -56,3 +56,27 @@ func (h *transactionHandler) GetUserTransaction(c *gin.Context) {
 	response := helper.APIResponse("Campaign transactions", http.StatusOK, "success", transaction.FormatUserTransactions(transactions))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *transactionHandler) CreateTransaction(c *gin.Context) {
+	var input transaction.CreateTransactionInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+
+	newTransaction, err := h.service.CreateTransaction(input)
+	if err != nil {
+		response := helper.APIResponse("Failed ASW", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create transactions", http.StatusOK, "success", transaction.FormatTransaction(newTransaction))
+	c.JSON(http.StatusOK, response)
+}
---
+++ payment/service.go
@@ -0,0 +1,47 @@
+package payment
+
+import (
+	"bwastartup/user"
+	"strconv"
+
+	"github.com/veritrans/go-midtrans"
+)
+
+type service struct {
+}
+
+type Service interface {
+	GetPaymentURL(transaction Transaction, user user.User) (string, error)
+}
+
+func NewService() *service {
+	return &service{}
+}
+
+func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error) {
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = "SB-Mid-server-2Hg4VE5MVeJgck8ZlHM1X3LV"
+	midclient.ClientKey = "SB-Mid-client-vP7hZdvPGmIrza6I"
+	midclient.APIEnvType = midtrans.Sandbox
+
+	snapGateway := midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapReq := &midtrans.SnapReq{
+		CustomerDetail: &midtrans.CustDetail{
+			Email: user.Email,
+			FName: user.Name,
+		},
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Amount),
+		},
+	}
+
+	snapTokenResp, err := snapGateway.GetToken(snapReq)
+	if err != nil {
+		return "", err
+	}
+	return snapTokenResp.RedirectURL, nil
+}
