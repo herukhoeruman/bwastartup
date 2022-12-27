1c4188eb3efd4386a7716ecb8fc465441cd8a972
herukhoeruman <herukhoeruman110@gmail.com>
2022-12-27 10:42:38 +0700

    create transaction midtrans

--- payment/service.go
+++ payment/service.go
@@ -20,8 +20,8 @@ func NewService() *service {
 
 func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error) {
 	midclient := midtrans.NewClient()
-	midclient.ServerKey = "SB-Mid-server-2Hg4VE5MVeJgck8ZlHM1X3LV"
-	midclient.ClientKey = "SB-Mid-client-vP7hZdvPGmIrza6I"
+	midclient.ServerKey = ""
+	midclient.ClientKey = ""
 	midclient.APIEnvType = midtrans.Sandbox
 
 	snapGateway := midtrans.SnapGateway{
